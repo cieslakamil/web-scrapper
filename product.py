e03c75357cefbb72cd21f73949149e8f6af0125d
Kamil Cieśla <kamilciesla34@gmail.com>
2021-02-28 22:47:49 +0100

    switched from opinion class to product class

---
+++ product.py
@@ -0,0 +1,40 @@
+class Product:
+    def __init__(self, scrapped_data):
+        self.scrapped_data = scrapped_data
+        self.opinions = [self.get_opinion(raw_opinion) for raw_opinion in self.scrapped_data.find_all(
+            class_='user-post user-post__card js_product-review')]
+
+    def get_opinion(self, raw_opinion):
+        opinion = {
+            'id': raw_opinion['data-entry-id'],
+            'author': raw_opinion.find(class_='user-post__author-name').text.strip("\n"),
+            'recommendation': raw_opinion.find(class_='recommended').text,
+            'score': raw_opinion.find(class_='user-post__score-count').text,
+            'is_confirmed': True if raw_opinion .find(class_='review-pz') else False,
+            'date': [time['datetime'] for time in raw_opinion.find(class_='user-post__published')('time')],
+            'votes_yes': raw_opinion.find(class_='vote-yes')['data-total-vote'],
+            'votes_no': raw_opinion.find(class_='vote-no')['data-total-vote'],
+            'contents': raw_opinion.find(class_='user-post__text').text,
+            # Below are three ways in which positives and negatives can be extracted:
+            # option 1
+            #'positives': [item.text for item in raw_opinion.find(class_='review-feature__title--positives').parent(class_='review-feature__item')] if raw_opinion.find(class_='review-feature__title--positives') else [],
+            #'negatives': [item.text for item in raw_opinion.find_all(class_='review-feature__item') if item.parent.find(class_='review-feature__title--negatives')]
+        }
+        # option 2
+        for col in raw_opinion(class_='review-feature__col'):
+            features = [item.text for item in col.find_all(
+                class_='review-feature__item')]
+            if col.find(class_='review-feature__title--positives'):
+                opinion['positives'] = features
+            elif col.find(class_='review-feature__title--negatives'):
+                opinion['negatives'] = features
+        # option 3
+        '''
+        if raw_opinion.find(class_='review-feature__title--positives'):
+            opinion['positives'] = [item.text for item in raw_opinion.find(
+                class_='review-feature__title--positives').parent(class_='review-feature__item')]
+        if raw_opinion.find(class_='review-feature__title--negatives'):
+            opinion['negatives'] = [item.text for item in raw_opinion.find(
+                class_='review-feature__title--negatives').parent(class_='review-feature__item')]
+        '''
+        return opinion
