b360bb6dd529411847fec386681f3c7e1aab1167
cieslakamil <kamilciesla34@gmail.com>
2021-03-02 15:35:06 +0100

    set up stats

--- main.py
+++ main.py
@@ -6,13 +6,11 @@ from bs4 import BeautifulSoup
 from product import Product
 
 base_url = 'https://www.ceneo.pl/'
-product_key = '85615932'
+product_key = '90654635'
 # Get content of site containing product opinions
 response = requests.get(base_url+product_key)
 
 with open('index.html') as file:
     soup = BeautifulSoup(response.text, 'html.parser')
-
 product = Product(soup)
-print(product.opinions[0]['positives'])
-print(product.opinions[0]['negatives'])
+print(product.average_score)
--- product.py
+++ product.py
@@ -3,18 +3,23 @@ class Product:
         self.scrapped_data = scrapped_data
         self.opinions = [self.get_opinion(raw_opinion) for raw_opinion in self.scrapped_data.find_all(
             class_='user-post user-post__card js_product-review')]
-
+        self.opinions_count = len(self.opinions)
+        self.positives_count = sum(len(opinion['positives']) for opinion in self.opinions)
+        self.negatives_count = sum(len(opinion['negatives']) for opinion in self.opinions)
+        self.average_score = sum(opinion['score'] for opinion in self.opinions)/self.opinions_count
     def get_opinion(self, raw_opinion):
         opinion = {
             'id': raw_opinion['data-entry-id'],
             'author': raw_opinion.find(class_='user-post__author-name').text.strip("\n"),
             'recommendation': raw_opinion.find(class_='recommended').text,
-            'score': raw_opinion.find(class_='user-post__score-count').text,
+            'score': float(raw_opinion.find(class_='user-post__score-count').text[:-2].replace(',','.')),
             'is_confirmed': True if raw_opinion .find(class_='review-pz') else False,
             'date': [time['datetime'] for time in raw_opinion.find(class_='user-post__published')('time')],
             'votes_yes': raw_opinion.find(class_='vote-yes')['data-total-vote'],
             'votes_no': raw_opinion.find(class_='vote-no')['data-total-vote'],
             'contents': raw_opinion.find(class_='user-post__text').text,
+            'positives': [],
+            'negatives': []
             # Below are three ways in which positives and negatives can be extracted:
             # option 1
             #'positives': [item.text for item in raw_opinion.find(class_='review-feature__title--positives').parent(class_='review-feature__item')] if raw_opinion.find(class_='review-feature__title--positives') else [],
