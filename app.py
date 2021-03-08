31449ea8b3023767f5a35c9d862130768dbcb40e
cieslakamil <kamilciesla34@gmail.com>
2021-03-08 20:34:08 +0100

    added download buttons

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, send_file
 from product import Product
 from data_conversion import dict_list_to_json, dict_list_to_csv, dict_list_to_xml
 
@@ -31,11 +31,19 @@ def get_opinions():
 
 @app.route('/product/<product_code>')
 def display_product(product_code):
-    product = Product(product_code) 
-    #dict_list_to_json(product.opinions, 'opinions.json')
-    #dict_list_to_csv(product.opinions, 'opinions.csv')
-    #dict_list_to_xml(product.opinions, 'opinions.xml')
+    product = Product(product_code)
+    # dict_list_to_json(product.opinions, 'opinions.json')
+    # dict_list_to_csv(product.opinions, 'opinions.csv')
+    # dict_list_to_xml(product.opinions, 'opinions.xml')
     return render_template('/product.html', product=product)
+
+
+@app.route('/download-opinions/<file_name>')
+def download_opinions(file_name):
+    try:
+        return send_file(file_name, attachment_filename=file_name)
+    except Exception as e:
+        return str(e)
 
 if __name__ == "__main__":
     app.run()
