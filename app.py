d5dc34945cbfc073fe8283c949e75772e583f1ca
cieslakamil <kamilciesla34@gmail.com>
2021-03-09 12:50:54 +0100

    xlsx conversion and dataconversion.py rework

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect, send_file
 from product import Product
-from data_conversion import dict_list_to_json, dict_list_to_csv, dict_list_to_xml
+from data_conversion import dict_list_to_file
 
 
 app = Flask(__name__)
@@ -40,16 +40,7 @@ def download_opinions(product_code, file_type):
     product = Product(product_code)
     file_name = f'{product_code}.{file_type}'
     file_path = f'./opinions/{product_code}'
-    if file_type == 'csv':
-        dict_list_to_csv(product.opinions, file_path, file_name)
-    if file_type == 'json':
-        dict_list_to_json(product.opinions, file_path, file_name)
-    if file_type == 'xlsx':
-        pass
-        #dict_list_to_xlsx(product.opinions, file_path, file_name)
-    if file_type == 'xml':
-        dict_list_to_xml(product.opinions, file_path, file_name)
-
+    dict_list_to_file(product.opinions, file_path, file_name, file_type)
     try:
         return send_file(file_path+'/'+file_name, as_attachment=True, attachment_filename=file_name)
     except Exception as e:
--- data_conversion.py
+++ data_conversion.py
@@ -1,28 +1,30 @@
 import json
 import csv
 from dicttoxml import dicttoxml
+import pandas
+import xlsxwriter
 # for creating a path if it does not exist
 import os
 
 
-def dict_list_to_json(dict_list, file_path, file_name):
-    """ Write to a json file from a list of dictionaries."""
+def dict_list_to_file(dict_list, file_path, file_name, file_format):
+    """ Write to chosen file format data from a list of dictionaries."""
     os.makedirs(file_path, exist_ok=True)
-    with open(file_path+'/'+file_name, 'w', encoding='utf-8') as file:
-        json.dump(dict_list, file, ensure_ascii=False)
+    if file_format == 'csv':
+        with open(file_path+'/'+file_name, 'w', encoding='utf-8', newline='') as file:
+            dict_writer = csv.DictWriter(file, dict_list[0].keys())
+            dict_writer.writeheader()
+            dict_writer.writerows(dict_list)
+    if file_format == 'json':
+        with open(file_path+'/'+file_name, 'w', encoding='utf-8') as file:
+            json.dump(dict_list, file, ensure_ascii=False)
+    if file_format == 'xlsx':
+        data_frame = pandas.DataFrame(dict_list)
+        xlsx_writer = pandas.ExcelWriter(
+            file_path+'/'+file_name, engine='xlsxwriter')
+        data_frame.to_excel(xlsx_writer, sheet_name='Sheet1')
+        xlsx_writer.save()
 
-
-def dict_list_to_csv(dict_list, file_path, file_name):
-    """ Write to a csv file from a list of dictionaries."""
-    os.makedirs(file_path, exist_ok=True)
-    with open(file_path+'/'+file_name, 'w', encoding='utf-8', newline='') as file:
-        dict_writer = csv.DictWriter(file, dict_list[0].keys())
-        dict_writer.writeheader()
-        dict_writer.writerows(dict_list)
-
-
-def dict_list_to_xml(dict_list, file_path, file_name):
-    """ Write to a xml file from a list of dictionaries."""
-    os.makedirs(file_path, exist_ok=True)
-    with open(file_path+'/'+file_name, 'w', encoding='utf-8') as file:
-        file.write(str(dicttoxml(dict_list, attr_type=False), 'utf-8'))
+    if file_format == 'xml':
+        with open(file_path+'/'+file_name, 'w', encoding='utf-8') as file:
+            file.write(str(dicttoxml(dict_list, attr_type=False), 'utf-8'))
